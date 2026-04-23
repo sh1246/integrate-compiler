6f49eccdd7ec06209556e2b3a4af948d99158ba8
sanghyun.park <dasa00@icloud.com>
2026-04-24 00:33:49 +0900

    [parser] Implement AST node sturcture

---
+++ include/dsl/AST.h
@@ -0,0 +1,51 @@
+//Define AST node for ipdef parser
+#pragma once
+#include <string>
+#include <vector>
+#include <cstdint>
+
+struct FieldNode {
+  std::string name; //Field name
+  //[Hi:Lo]
+  int bitLo;
+  int bitHi;
+  std::string desc; //Description
+};
+
+//Single Register format
+struct RegmapNode {
+  std::string label;
+  uint32_t offset;
+  uint32_t size;
+  std::string access; //"RW or RO or WO"
+  uint32_t resetValue;
+  std::vector<FieldNode> fields;
+};
+
+//Ports
+struct PortNode {
+  std::string name;
+  std::string portType;  //AXI-L, AXIS, AXI, wire
+  std::string direction; //in, out, slave, master
+  int dataWidth = 0;     //No data width
+  int addrWidth = 0;     //Used in AMBA protocol
+};
+
+//Clock
+struct ClockPortNode {
+  std::string name;
+  uint64_t frequency = 0;
+  std::string domain; //Clock-domain name used in fabric
+};
+
+//IP-definitino
+struct IPDefNode {
+  std::string compatible;   //Unique Identifier of IP e.g, mb, uart-v2
+  int dataWidth = 32;
+  int addrWidth = 8;
+  std::vector<RegmapNode> regmaps;
+  std::vector<PortNode>   ports;
+  std::vector<ClockPortNode> clocks;
+  //Debug
+  void dump() const;
+};
---
+++ src/dsl/AST.cpp
@@ -0,0 +1,18 @@
+//
+#include "dsl/AST.h"
+
+//DUmp for debugging
+void IPDefNode::dump() const {
+  std::printf("\n===IPDefNode===\n");
+  std::printf("compatible : %s\n", compatible.c_str());
+  std::printf("dataWidth : %0d\n", dataWidth);
+  std::printf("\nClocks (%zu):\n", clocks.size());
+  for (auto& itr : clocks)
+    std::printf("%s,Freq=%llu,Domain=%s\n",itr.name.c_str(), itr.frequency, itr.domain.c_str());
+  std::printf("\nregmap (%zu):\n",regmaps.size());
+  for (auto& r : regmaps) {
+    std::printf("\t[0x%04X]:%s,access=%s,reset=0x%08X\n", r.offset, r.label.c_str(), r.access.c_str(), r.resetValue);
+    for (auto& f : r.fields)
+      std::printf("\t\t[%2d:%2d]:%s,\"%s\"\n",f.bitHi, f.bitLo, f.name.c_str(), f.desc.c_str());
+  }
+}
